2d384f736328c723117f2b425654d5e7d4fccd84
Will Furnass <will@thearete.co.uk>
2018-02-07 20:56:29 +0000

    Update OpenMPI build recipe for v3.0.0

--- openmpi/build.sh
+++ openmpi/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -e
+
+# Build OpenMPI with support for Intel Omni-Path Architecture high-speed interconnects (via the PSM2 driver)
 ./configure --prefix=$PREFIX --with-psm2
-make -j 16
+# Build in parallel if NSLOTS is defined
+make -j ${NSLOTS-1}
 make check
 make install
