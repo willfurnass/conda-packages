f43185a10ece4ab2192d6259ab7f1d05dc06638f
W. Furnass <will@thearete.co.uk>
2018-02-08 17:48:43 +0000

    Revise so based on conda-forge recipe but use anaconda compilers

--- openmpi/build.sh
+++ openmpi/build.sh
@@ -3,9 +3,26 @@
 # Exit if any command fails
 set -e
 
-# Build OpenMPI with support for Intel Omni-Path Architecture high-speed interconnects (via the PSM2 driver)
-./configure --prefix=$PREFIX --with-psm2
-# Build in parallel if NSLOTS is defined
-make -j ${NSLOTS-1}
-make check
-make install
+export LIBRARY_PATH="$PREFIX/lib"
+
+# Open MPI now ignores the F77 and FFLAGS environment variables; only the FC and FCFLAGS environment variables are used. 
+unset F77 
+unset FFLAGS 
+
+# Build OpenMPI with support for 
+# - Intel Omni-Path Architecture high-speed interconnects (via the PSM2 driver)
+# - C and Fortran but not C++
+#./configure --prefix=$PREFIX \
+#            --disable-dependency-tracking \
+#            --enable-mpi-fortran \
+#            --with-psm2
+## Build in parallel if NSLOTS is defined
+#make -j ${NSLOTS-1}
+#make check
+#make install
+if type ldconfig 2>&1 > /dev/null; then
+    ldconfig -p
+else
+    echo "SYSTEM LDCONFIG"
+    /sbin/ldconfig -p
+fi
